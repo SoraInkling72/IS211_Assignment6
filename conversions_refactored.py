ea8f740165a93a3c90fd527254396ecf278fac18
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2023-10-09 23:51:09 -0400

    What is the tests.py supposed to contain?

--- conversions_refactored.py
+++ conversions_refactored.py
@@ -1,4 +1,5 @@
 import conversions
+import tests
 
 def fromUnit:
     '''convert one unit of measurement into another'''
@@ -8,8 +9,25 @@ def fromUnit:
         raise ImproperCalculationError ('formula is incorrect')
     if not (fahrenheit = fahrenheit):
         raise ImproperCalculationError ('formula is incorrect')
+    if not (meters = meters)
+        raise ImproperCalculationError ('formula is incorrect')
+    if not (yards = yards)
+        raise ImproperCalculationError('formula is incorrect')
+    if not (miles = miles)
+        raise ImproperCalculationError('formula is incorrect')
 
 def toUnit:
-
+    if not (kelvins = kelvins):
+        raise ImproperCalculationError ('formula is incorrect')
+    if not (celsius = celsius):
+        raise ImproperCalculationError ('formula is incorrect')
+    if not (fahrenheit = fahrenheit):
+        raise ImproperCalculationError ('formula is incorrect')
+    if not (meters = meters)
+        raise ImproperCalculationError ('formula is incorrect')
+    if not (yards = yards)
+        raise ImproperCalculationError('formula is incorrect')
+    if not (miles = miles)
+        raise ImproperCalculationError('formula is incorrect')
 
 def value:
--- tests.py
+++ tests.py
@@ -1,17 +1,40 @@
 import unittest
 class meters(unittest.TestCase):
     #Meters can be any number, but I chose this unit as it's easier to use for smaller units#
-    known_values = (29,
-                    33,
-                    15,
-                    8,
-                    24)
+    known_values = (345,
+                    12866,
+                    18,
+                    577,
+                    248752879)
 
 def test_to_measurments_known_values(self):
-    '''to_roman should give known result with known input'''
     for integer, number in self.known_values:
         result = integer
         self.assertEqual(result)
 
 if __name__ == '__main__':
     unittest.main()
+
+def convertMetersToYards(meters):
+    yards = (meters/0.9144)
+    return yards
+
+def convertMetersToMiles(meters):
+    miles = (meters/1609.344)
+    return miles
+
+def convertYardsToMeters(yards):
+    meters = (yards*0.9144)
+    return meters
+
+def convertYardsToMiles(yards):
+    miles = (yards/1760)
+    return miles
+
+def convertMilesToYards(miles):
+    yards = (miles*1760)
+    return yards
+
+def convertMilesToMeters(miles):
+    meters = (miles*1609.344)
+    return meters
