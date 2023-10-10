43b9d681d946f8610597958ece90825696bf5d29
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2023-10-09 22:02:47 -0400

    Work in progress

--- conversions.py
+++ conversions.py
@@ -1,11 +1,20 @@
 import unittest
-class Celsius(unittest.TestCase):
+class celsius(unittest.TestCase):
     #Celsius temperatures can be any number, but I chose numbers that are bearable for an environment#
-    celsius = (29,
-               33,
-               15,
-               8,
-               24)
+    known_values = (29,
+                    33,
+                    15,
+                    8,
+                    24)
+
+def test_to_temperature_known_values(self):
+    '''to_roman should give known result with known input'''
+    for integer, numeral in self.known_values:
+        result = roman1.to_roman(integer)
+        self.assertEqual(numeral, result)
+
+if __name__ == '__main__':
+    unittest.main()
 
 def convertCelsiusToKelvin(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
